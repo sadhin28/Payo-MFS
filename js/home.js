e11f478e56b1b0fe4f36bc788110f2dae6da4b6a
sadhin28 <taosifsadhin113@gmail.com>
2025-04-09 02:53:14 +0600

    add money update

--- js/home.js
+++ js/home.js
@@ -2,13 +2,20 @@
 document.getElementById('add-Money')
     .addEventListener('click', function (event) {
         event.preventDefault();
+        const bankacount=document.getElementById('bankNumber');
         const pin = document.getElementById('pinNumber').value;
-       
+       //varify pin number
         if ( pin === '1234') {
            const amount=document.getElementById('addAmount').value;
-            console.log(amount)
-       
+           const blance= document.getElementById('blance').innerText;
+           
+           const amnt=parseFloat(amount);
+           const blnc=parseFloat(blance);
+           const newBlance= blnc+amnt;
+           console.log(newBlance);
+            //step 6: update
+           document.getElementById('blance').innerText= newBlance;
         } else {
-            alert('Worng Bank acount and Pin')
+            alert('Faild to add money,Try again')
         }
     })
