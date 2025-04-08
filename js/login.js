eb29f697c4a222faefdfca8ec558675f57811978
sadhin28 <taosifsadhin113@gmail.com>
2025-04-08 23:29:01 +0600

    add payooo

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ document.getElementById('loginButton')
         const phoneNumber = document.getElementById('phone-number').value;
         const pinNumber = document.getElementById('pass').value;
         if (phoneNumber === '1234' && pinNumber === '12') {
-            console.log('You are loging')
+           window.location.href= '/home.html'
         } else {
             alert('Worng phone Number Or pin')
         }
