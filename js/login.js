a2a72f2d61cc9d87329d1761731b56977e916965
sadhin28 <taosifsadhin113@gmail.com>
2025-04-08 19:52:40 +0600

    add input field

--- js/login.js
+++ js/login.js
@@ -1,20 +1,13 @@
-//search:form submit reloading the page
+// search:form submit reloading the page
 
 document.getElementById('loginButton')
-    .addEventListener('click',
-        function (event) {
-            event.preventDefault();
-          const phoneNumber=document.getElementById('phone-number').value;
-          
-          const pinNumber=document.getElementById('pass').value;
-          
-          if( phoneNumber === '01707986054' && pinNumber === '1234'){
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const phoneNumber = document.getElementById('phone-number').value;
+        const pinNumber = document.getElementById('pass').value;
+        if (phoneNumber === '1234' && pinNumber === '12') {
             console.log('You are loging')
-          }
-          else{
-            alert("Worng Phone number Or pin")
-          }
-         
-            
+        } else {
+            alert('Worng phone Number Or pin')
         }
-    )
+    })
