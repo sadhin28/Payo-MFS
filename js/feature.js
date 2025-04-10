203ef2522e8a7ef11cf43b9de50cb49d0387ee27
sadhin28 <taosifsadhin113@gmail.com>
2025-04-10 23:30:19 +0600

    sadhin

--- js/feature.js
+++ js/feature.js
@@ -1,27 +1,24 @@
 document.getElementById('addMoney').addEventListener('click', function () {
-
-   document.getElementById('Add-money').classList.remove('hidden');
-   document.getElementById('cash-out').classList.add('hidden')
-   document.getElementById('TransferMoney').classList.add('hidden');
+   showSectionById('Add-money')
+  
 
 })
 document.getElementById('Cashout').addEventListener('click', function () {
-
-   document.getElementById('cash-out').classList.remove('hidden');
-   document.getElementById('Add-money').classList.add('hidden')
-   document.getElementById('TransferMoney').classList.add('hidden');
+   showSectionById('cash-out')
+  
 })
 document.getElementById('transferMoney').addEventListener('click', function () {
-
-   document.getElementById('TransferMoney').classList.remove('hidden');
-   document.getElementById('cash-out').classList.add('hidden')
-   document.getElementById('Add-money').classList.add('hidden')
-
+   showSectionById('TransferMoney')
+  
+  
 })
+
+
 
 function showSectionById(id){
    document.getElementById('TransferMoney').classList.add('hidden');
    document.getElementById('cash-out').classList.add('hidden')
    document.getElementById('Add-money').classList.add('hidden')
    document.getElementById(id).classList.remove('hidden')
+   
 }
