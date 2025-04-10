26218f3fe0de1be3c552e9f4eb173b3e8549a547
sadhin28 <taosifsadhin113@gmail.com>
2025-04-10 22:13:34 +0600

    add transfer money lable

--- js/feature.js
+++ js/feature.js
@@ -2,22 +2,20 @@ document.getElementById('addMoney').addEventListener('click', function () {
 
    document.getElementById('Add-money').classList.remove('hidden');
    document.getElementById('cash-out').classList.add('hidden')
+   document.getElementById('TransferMoney').classList.add('hidden');
 
 })
 document.getElementById('Cashout').addEventListener('click', function () {
 
    document.getElementById('cash-out').classList.remove('hidden');
    document.getElementById('Add-money').classList.add('hidden')
+   document.getElementById('TransferMoney').classList.add('hidden');
+})
+document.getElementById('transferMoney').addEventListener('click', function () {
+
+   document.getElementById('TransferMoney').classList.remove('hidden');
+   document.getElementById('cash-out').classList.add('hidden')
+   document.getElementById('Add-money').classList.add('hidden')
 
 })
 
-
-
-// function cashO(){
-//     let x=document.getElementById('cash-out');
-//     if(x.style.display === "none"){
-//         x.style.display='block';
-//     }else{
-//         x.style.display='none';
-//     }
-// }
