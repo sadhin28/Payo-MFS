6a1628c27738afe1ecba08a2fe834ffbc27fdf7f
sadhin28 <taosifsadhin113@gmail.com>
2025-04-10 22:46:47 +0600

    add pin

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ document.getElementById('loginButton')
         event.preventDefault();
         const phoneNumber = Addbyid('phone-number');
         const pinNumber =  Addbyid('pass');
-        if (phoneNumber === '1234' && pinNumber === '12') {
+        if (phoneNumber === '1234' && pinNumber === '1234') {
             window.location.href = '/home.html'
         } else {
             alert('Worng phone Number Or pin')
